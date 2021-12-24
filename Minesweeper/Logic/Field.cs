81973f777b557d6be06892f923dc0c40a38aaccf
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2021-12-24 02:13:13 +0100

    Add Field and Level classes

--- Minesweeper/Logic/Field.cs
+++ Minesweeper/Logic/Field.cs
@@ -8,5 +8,38 @@ namespace Minesweeper.Logic
 {
     public class Field
     {
+        #region Attributes
+        private int x;
+        private int y;
+        private bool isMine;
+        private bool isOpened;
+        private bool isMarked;
+        #endregion
+
+        #region Constructor
+        public Field(int x, int y, bool isMine)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        #endregion
+
+        #region Getters
+        public int getX() { return this.x; }
+
+        public int getY() { return this.y; }
+
+        public bool getIsMine() { return this.isMine; }
+
+        public bool getIsOpened() { return this.isOpened; }
+
+        public bool getIsMarked() { return this.isMarked; }
+        #endregion
+
+        #region Setters
+        public void setIsMine(bool isMine) { this.isMine = isMine; }
+
+        public void setIsOpened(bool isOpened) { this.isOpened = isOpened; }
+        #endregion
     }
 }
--- Minesweeper/Logic/Level.cs
+++ Minesweeper/Logic/Level.cs
@@ -8,5 +8,24 @@ namespace Minesweeper.Logic
 {
     public class Level
     {
+        #region Attributes
+        private int numberOfMines;
+        private int boardSize;
+        #endregion
+
+        #region Constructor
+        public Level(int numberOfMines, int boardSize)
+        {
+            this.numberOfMines = numberOfMines;
+            this.boardSize = boardSize;
+        }
+        #endregion
+
+        #region Getters
+        public int getNumberOfMines() { return this.numberOfMines; }
+
+        public int getBoardSize() { return this.boardSize; }
+        #endregion
+
     }
 }
